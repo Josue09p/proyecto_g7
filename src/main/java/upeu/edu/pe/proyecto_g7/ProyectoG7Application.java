ee7b665a9a1f60af58439214f75df1718b7c52ef
LAB-2 <LAB-2@LAB2-28>
2023-08-14 12:14:52 -0500

    holaaa xd

---
+++ src/main/java/upeu/edu/pe/proyecto_g7/ProyectoG7Application.java
@@ -0,0 +1,13 @@
+package upeu.edu.pe.proyecto_g7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoG7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoG7Application.class, args);
+	}
+
+}
---
+++ src/test/java/upeu/edu/pe/proyecto_g7/ProyectoG7ApplicationTests.java
@@ -0,0 +1,13 @@
+package upeu.edu.pe.proyecto_g7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoG7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
